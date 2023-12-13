413ec6300f4462adce0b77678ee881b59889e4f8
samkalvium <samarth.s@kalvium.community>
2023-12-13 18:22:22 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant
     },
     {
-      id:4,
-      img:elephant
+      id: 4,
+      img: elephant
     }
   ]
   return data;
@@ -25,6 +25,20 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  let imgg = imageData();
+
+  return (
+    <div>
+      <h1>Kalvium Gallery</h1>
+      <div className='body'>
+        {imgg.map((ob) => {
+          return (
+            <img src={ob.img} alt='elephant' />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -2,29 +2,46 @@ import { Component } from "react";
 import "./App.css"
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant
       },
       {
-        id:4,
-        img:elephant
+        id: 4,
+        img: elephant
       }
     ]
     return data;
   }
 
   // code here
+  render() {
+    let imgData = this.imageData();
+
+    return (
+      <div>
+        <h1>Kalvium Gallery</h1>
+        <div className='body'>
+          {imgData.map((ob) => {
+            return (
+              <img src={ob.img} alt='elephant' />
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
 }
